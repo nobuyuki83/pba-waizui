8d5ce1e6318000348eb089cb04d301aa21033518
Nobuyuki Umetani <n.umetani@gmail.com>
2025-04-28 04:29:13 +0900

    added task01

---
+++ task01/README.md
@@ -0,0 +1,94 @@
+# Task01: Implicit Time Integration
+
+![preview](thumbnail.gif)
+
+**Deadline: May 1st (Thu) at 15:00pm**
+
+----
+
+## Before Doing Assignment
+
+If you have not done the [task00](../task00), do it first to set up the rust development environment.
+
+Before doing the assignment `task01`, we need to update your remote/local repositories.     
+1. Go to your remote GitHub repository at `https://github.com/PBA-2025/pba-<username>`. 
+2. Click `Pull Requests` tab around the top.
+3. There should be one pull request with name `GitHub Classroom: Sync Assignment`, click it.
+4. Click the green `Merge pull request` button.
+
+Now your remote repository is updated. Let's update your local repository. 
+
+```bash
+$ cd pba-<username>  # go to the local repository
+$ git checkout main  # set main branch as the current branch
+$ git branch -a       # make sure you are in the main branch
+$ git fetch origin main  # download the main branch from the remote repository
+$ git reset --hard origin/main # reset the local main branch same as remote repository
+```
+
+If you encounter some errors, give up updating your local repository. 
+Instead, clone the remote repository in a new location (see [task00](../task00) for cloning).      
+
+Create the `task01` branch and set it as the current branch.
+
+```bash
+$ git branch task01    # create task1 branch
+$ git checkout task01  # switch into the task1 branch
+$ git branch -a       # make sure you are in the task1 branch
+```
+
+Now you are ready to go!
+
+---
+
+## Problem 0
+
+Run the code
+
+```bash
+cargo run --release
+```
+Take a screenshot image (looks like image at the top)
+
+![problem0](problem0.gif)
+
+This program simulates the motion of small objects under gravity. 
+It solves the equation of motion ${d^2 r}/{d t^2} = - {1}/{r^2}$ and visualize it in the polar coordinate.
+Note that the equation is 1D. The angle of the polar coordinate represent time.    
+The falling circle bounce at the surface $r=0.5$. 
+The movement of ***red circle*** is integrated using the ***forward time integration***. 
+Observe the energy gain artifact.
+
+## Problem 2
+
+Modify the code at `line #43`  and `line #44` in [src/main.rs](src/main.rs) to integrate the equation of motion using the ***Backward(Implicit) Euler Method*** scheme for the movement of ***Red object***. 
+
+`task01/problem1.gif`
+
+![problem1](problem1.gif)
+
+
+## After Doing the Assignment
+
+After modifying the code, push the code in the `task01` branch to the `task01` branch of the remote repository.
+
+```bash
+$ cd pba-<username>    # Go to the top of the repository
+$ git branch -a  # Make sure again you are in the task1 branch
+$ git status  # check the changes (typically few files are shown to be "updated")
+$ git add .   # stage the changes
+$ git status  # check the staged changes (typically few files are shown to be "staged")
+$ git commit -m "task01 finished"   # The comment can be anything
+$ git push --set-upstream origin task01  # update the task1 branch of the remote repository
+```
+
+got to the GitHub webpage `https://github.com/PBA-2025/pba-<username>`. 
+If everything looks good on this page, make a pull request.
+![](../doc/pullrequest.png)
+
+## Notes
+
+- Do not submit multiple pull requests. Only the first pull request is graded
+- Do not close the pull request by yourself. The instructor will close the pull request
+- If you mistakenly merge the pull request, it's OK, but be careful not to merge next time. 
+
---
+++ task01/src/main.rs
@@ -0,0 +1,192 @@
+/**
+ * Task01:  Implicit Time Integration
+ */
+
+/// Explicit time integration
+///
+/// # Argument
+/// * `rv0` - pair of altitude from the center of the planet and its velocity
+/// * `dt` - time step
+fn time_integration_explicit(rv0: &mut (f32, f32), dt: f32) {
+    let r0 = rv0.0; // current altitude
+    let v0 = rv0.1; // current altitude velocity
+    let f0 = -1f32 / (r0 * r0); // force based on the **current** altitude
+    *rv0 = (r0 + dt * v0, v0 + dt * f0);
+}
+
+/// inverse of 2x2 matrix
+/// # Argument
+/// * `a` : 2x2 matrix with `f32` elements
+///
+/// # Return
+/// Option of unversed 2x2 matrix. The option is `None` is the input is singular.
+/// Read https://doc.rust-lang.org/rust-by-example/std/option.html for the details of the Option syntax.
+fn inverse_matrix_2x2(a: &[[f32; 2]; 2]) -> Option<[[f32; 2]; 2]> {
+    let det = a[0][0] * a[1][1] - a[0][1] * a[1][0];
+    if det == 0.0 {
+        return None;
+    }
+    let inv_det = 1.0 / det;
+    Some([
+        [a[1][1] * inv_det, -a[0][1] * inv_det],
+        [-a[1][0] * inv_det, a[0][0] * inv_det],
+    ])
+}
+
+/// matrix-vector multiplication for 2x2 matrix.
+fn mult_mat2_vec(a: &[[f32; 2]; 2], b: &[f32; 2]) -> [f32; 2] {
+    [
+        a[0][0] * b[0] + a[0][1] * b[1],
+        a[1][0] * b[0] + a[1][1] * b[1],
+    ]
+}
+
+/// Implicit time integration
+///
+/// # Argument
+/// * `rv0` - pair of altitude from the center of the planet and its velocity
+/// * `dt` - time step
+fn time_integration_implicit(rv0: &mut (f32, f32), dt: f32) {
+    let r0 = rv0.0;
+    let v0 = rv0.1;
+    // ----------------------
+    // write some code below
+
+    let dfdr = 2f32 / (r0 * r0 * r0); // hint!
+                                      // let a_mat = [[???, ???], [???, ???]]; // hint
+                                      // let b_vec = [???, ???]; // hint
+                                      // let a_mat_inv = inverse_matrix_2x2(&a_mat).unwrap(); // hint
+                                      // let res = mult_mat2_vec(&a_mat_inv, &b_vec); // hint
+                                      // *rv0 = (res[0], res[1]); // hint
+
+    *rv0 = (r0, v0); // delete this line
+
+    // no further edit from here
+    // ----------------------
+}
+
+/// When the center of the ball is below the surface of the planet,
+/// we set the ball at the surface while setting the velocity such that there is no energy loss
+fn reflection(rv0: &mut (f32, f32)) {
+    if rv0.0 > 0.5f32 {
+        return;
+    }
+    let r0 = rv0.0;
+    let v0 = rv0.1;
+    let energy0 = 0.5f32 * v0 * v0 - 1f32 / r0; // energy before reflection
+    let r1 = 0.5f32;
+    let v1 = (2f32 * energy0 + 4f32).max(0.).sqrt();
+    let energy1 = 0.5f32 * v1 * v1 - 1f32 / r1; // energy before reflection
+
+    // dbg!(energy1, energy0);
+
+    *rv0 = (r1, v1); // energy preserving reflection
+}
+
+fn draw_falling_object(
+    pix2coloridx: &mut [u8],
+    img_width: usize,
+    pos_explicit: &[f32; 2],
+    trajectory: &[[f32; 2]],
+    transform_ndc2pix: &[f32; 9],
+    coloridx: u8,
+) {
+    del_canvas::rasterize::circle2::stroke_dda(
+        pix2coloridx,
+        img_width,
+        pos_explicit,
+        0.05,
+        transform_ndc2pix,
+        coloridx,
+    );
+    for i_frame in 0..trajectory.len() - 1 {
+        del_canvas::rasterize::line2::draw_dda(
+            pix2coloridx,
+            img_width,
+            &trajectory[i_frame],
+            &trajectory[i_frame + 1],
+            transform_ndc2pix,
+            coloridx,
+        );
+    }
+}
+
+fn main() -> anyhow::Result<()> {
+    println!("Task01: Implicit Time Integration");
+    let img_size = 300;
+    // define canvas for gif animation
+    let mut canvas = del_canvas::canvas_gif::Canvas::new(
+        "problem0.gif",
+        (img_size, img_size),
+        &[
+            0xffffff, // color_0
+            0x000000, // color_1
+            0xff0000, // color_2
+            0x0000ff, // color_3
+        ],
+    )?;
+    // mapping from normalized device coordinate (NDC), i.e., [-1,+1]^2 to pixel coordinate, i.e., (0,0)x(W,H).
+    // This affine transformation matrix is stored in the column major order
+    let transform_ndc2pix = [
+        img_size as f32 * 0.5,
+        0.,
+        0.,
+        0.,
+        -(img_size as f32) * 0.5,
+        0.,
+        0.5 * (img_size as f32),
+        img_size as f32 * 0.5,
+        1.,
+    ];
+    let mut rv_explicit: (f32, f32) = (0.7, 0.0); // altitude & velocity for explicit time integration
+    let mut trajectory_explicit: Vec<[f32; 2]> = vec![];
+    let mut rv_implicit: (f32, f32) = (0.7, 0.2); // altitude & velocity for implicit time integration
+    let mut trajectory_implicit: Vec<[f32; 2]> = vec![];
+    let dt = 0.02;
+    let mut time = 0f32;
+    for i_frame in 0..1000 {
+        //
+        time_integration_explicit(&mut rv_explicit, dt);
+        reflection(&mut rv_explicit);
+        let pos_explicit = [rv_explicit.0 * time.cos(), rv_explicit.0 * time.sin()];
+        trajectory_explicit.push(pos_explicit);
+        //
+        time_integration_implicit(&mut rv_implicit, dt);
+        reflection(&mut rv_implicit);
+        let pos_implicit = [rv_implicit.0 * time.cos(), rv_implicit.0 * time.sin()];
+        trajectory_implicit.push(pos_implicit);
+        time += dt;
+        // ------------------------------
+        // visualization start from here
+        if i_frame % 10 == 0 {
+            canvas.clear(0); // clear with color_0 (white)
+                             // draw planet at the center
+            del_canvas::rasterize::circle2::stroke_dda(
+                &mut canvas.data,
+                canvas.width,
+                &[0.0, 0.0],
+                0.5,
+                &transform_ndc2pix,
+                1, // color_1 (black)
+            );
+            draw_falling_object(
+                &mut canvas.data,
+                canvas.width,
+                &pos_explicit,
+                &trajectory_explicit,
+                &transform_ndc2pix,
+                2,
+            ); // color_2 ()
+            draw_falling_object(
+                &mut canvas.data,
+                canvas.width,
+                &pos_implicit,
+                &trajectory_implicit,
+                &transform_ndc2pix,
+                3,
+            );
+            canvas.write();
+        }
+    }
+    Ok(())
+}
